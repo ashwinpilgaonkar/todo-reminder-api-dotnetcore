32f6d1f437b3de12068a8c9067ca661bf99a893f
Ashwin Pilgaonkar <ashwinpilgaonkar@gmail.com>
2024-04-10 13:29:48 -0400

    Set up Controllers

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todo_reminder_api_dotnetcore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoardsController : ControllerBase
+    {
+        // GET: api/<BoardsController> -- List all Boards
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Board 1", "Board 2" };
+        }
+
+        // POST api/<BoardsController> -- Add a new Board
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Console.WriteLine($"Added new Board {value}");
+        }
+
+        // PUT api/<BoardsController>/5 -- Edit a Board
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            Console.WriteLine($"Modified board {id} with {value}");
+        }
+
+        // DELETE api/<BoardsController>/5 -- Remove a Board
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Console.WriteLine($"Deleted board {id}");
+        }
+    }
+}
---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todo_reminder_api_dotnetcore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        // GET api/<ToDoController>/5 -- Get all To-Dos on a Specific Board
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Board {id}";
+        }
+
+        // POST api/<ToDoController> -- Add a new To-Do to a Board
+        [HttpPost]
+        public void Post([FromBody] string value, int boardId)
+        {
+            Console.WriteLine($"Added new To-Do {value} to Board {boardId}");
+        }
+
+        // PUT api/<ToDoController>/5 -- Edit a To-Do
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            Console.WriteLine($"Modified To-Do {id} with {value}");
+        }
+
+        // DELETE api/<ToDoController>/5 -- Remove a To-Do
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Console.WriteLine($"Deleted To-Do {id}");
+        }
+    }
+}
